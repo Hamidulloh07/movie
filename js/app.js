82c556652158b0151ea57c205635c5bd6da6eb1c
Hamidulloh07 <hsiddiqxojayev2005@gmail.com>
2022-08-30 20:19:34 +0600

    add

--- js/app.js
+++ js/app.js
@@ -4,18 +4,23 @@ let elTemplate = document.querySelector("#movies__template").content;
 let elList = document.querySelector(".movies__list");
 let elItem = document.querySelector(".movies__item");
 
+movies = movies.slice(0, 80);
+
 let newArr = [];
 
 let elMoviesArr = movies.forEach(movie => {
   newArr.push({
     title: movie.fulltitle,
-    reting: `Reting film:  ${movie.imdb_rating}`,
     img: `http://i3.ytimg.com/vi/${movie.ytid}/hqdefault.jpg`,
     category: `<b>Categorys:</b> ${movie.Categories}`,
     summary: movie.summary,
-    ytid: movie.ytid
+    ytid: movie.ytid,
+    reting: `Reting film:  ${movie.imdb_rating}`,
+    newRaiting: movie.imdb_rating
   })
 })
+
+movies.slice(0, 100)
 
 let createElement = (movie) => {
   let newItem = elTemplate.cloneNode(true);
@@ -59,6 +64,7 @@ elSelect.addEventListener("change", () => {
     renderFilms(filt);
   };
 });
+let arrExtra = newArr.slice(0, 80);
 
 elInput.oninput = () => {
   let inputValue = elInput.value;
@@ -66,3 +72,32 @@ elInput.oninput = () => {
   let filmName = newArr.filter(movie => regExp.test(movie.title));
   renderFilms(filmName);
 }
+let elSortSelect = document.querySelector(".extra-select");
+
+elSortSelect.addEventListener("change", function () {
+  if (this.value == "default-option") {
+    renderFilms(arrExtra);
+  } else if (this.value == "a-z-option") {
+    newArr = newArr.sort((a, b) => {
+      let movieName1 = a.title.toLowerCase();
+      let movieName2 = b.title.toLowerCase();
+      if (movieName1 < movieName2) return -1
+      return 1
+    });
+    renderFilms(newArr);
+  } else if (this.value == "z-a-option") {
+    newArr = newArr.sort((a, b) => {
+      let movieName3 = a.title.toLowerCase();
+      let movieName4 = b.title.toLowerCase();
+      if (movieName3 < movieName4) return 1
+      return -1
+    });
+    renderFilms(newArr);
+  } else if (this.value == "option-0-10") {
+    newArr = newArr.sort((a, b) => a.newRaiting - b.newRaiting);
+    renderFilms(newArr);
+  } else if (this.value == "option-10-0") {
+    newArr = newArr.sort((a, b) => b.newRaiting - a.newRaiting);
+    renderFilms(newArr);
+  }
+})
